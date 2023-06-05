5371b8b22d09c6561402d5fe14cf676d81892b83
Wu Zhouqin <wu_zhouqin@163.com>
2023-06-05 11:33:59 +0800

    polish prompt

--- fastchat/serve/serve_chatglm.py
+++ fastchat/serve/serve_chatglm.py
@@ -124,7 +124,13 @@ def chatglm_generate_stream(model, t2v_models, milvus_collections, sentences, to
     if education_result == 1:
         embeddings = t2v_models[1].encode([query])
         content = search(milvus_collections[1], 'vector', 'content', embeddings).entity.get('content')
-        query = '请总结下面内容，尽量简洁，50个字以内。\n\n```\n' + content + '\n```'
+        query = f'''请总结下面内容来回答问题，尽量简洁，50个字以内。材料以“```”开始和结束，问题以“///”开始和结束。
+
+        材料：
+        ```{content}```
+        问题：
+        ///{query}///
+        '''
         logging.warning("education query is:" + query)
 
     for response, new_hist in model.stream_chat(tokenizer, query, hist):
